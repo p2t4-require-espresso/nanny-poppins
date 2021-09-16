8276a9129e0a169eaabf1d8df49302ad6e7a8542
Janet Iqal <j.iqal35@gmail.com>
2021-09-16 09:44:40 -0500

    Revert "CRUD on user routes almost done, updated rating model column and rati…"

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,140 +1,61 @@
 const router = require('express').Router();
 const { User , Rating} = require('../../models');
-const withAuth= require("../../utils/auth")
 
-//GET ALL USERS 
-router.get('/', async (req,res)=>{
-  try{
-    const userData= await User.findAll({
-      include:[{
-        model:Rating
-      }]
-    })
-    res.status(200).json(userData);
-  }
-  catch(err){
-    res.status(500).json(err)
-  }
-})
-//gets all nanny users
-router.get('/nannies', async (req,res)=>{
-  try{
-    const userData= await User.findAll({
-      where:{
-        user_type:["nanny"]
-      },
-      include:[{
-        model:Rating
-      }]
-    })
-    res.status(200).json(userData);
-  }
-  catch(err){
-    res.status(400).json(err)
-  }
-})
-//gets all users: parents
-router.get('/parents', async (req,res)=>{
-  try{
-    const userData= await User.findAll({
-      where:{
-        user_type:["parent"]
-      },
-      include:[{
-        model:Rating
-      }]
-    })
-    res.status(200).json(userData);
-  }
-  catch(err){
-    res.status(400).json(err)
-  }
-})
-
-//finding one USER
-router.get('/:id', async (req,res)=>{
-  try{
-    const oneUser= await User.findByPk(req.params.id)
-    res.status(200).json(oneUser)
-  }
-  catch(err){
-    res.status(400).json(err)
-  }
-})
-//CREATES A NEW USER
-router.post('/', async (req, res)=>{
-  try{
+router.post('/', async (req, res) => {
+  try {
     const userData = await User.create(req.body);
-      
-      req.session.save(() => {
-      req.session.user_type = userData.user_type;
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
       req.session.logged_in = true;
 
       res.status(200).json(userData);
-  });
+    });
+  } catch (err) {
+    res.status(400).json(err);
   }
-  catch(err){
-    res.status(400).json(err)
-  }
-})
+});
 
-//update USER IF LOGGED IN!!!!!
-router.put('/:id', withAuth, async (req, res)=>{
-  try{
-    const userData = await User.update(req.body,{
-      where:{
-        id: req.params.id
-      }
-      //finish writing this route.....
-    })
-  }
-  
-  catch(err){
-    res.status(400).json(err)
-  }
-})
- 
+router.post('/login', async (req, res) => {
+  try {
+    const userData = await User.findOne({ where: { email: req.body.email } });
 
-// router.post('/login', async (req, res) => {
-//   try {
-//     const userData = await User.findOne({ where: { email: req.body.email } });
+    if (!userData) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
 
-//     if (!userData) {
-//       res
-//         .status(400)
-//         .json({ message: 'Incorrect email or password, please try again' });
-//       return;
-//     }
+    const validPassword = await userData.checkPassword(req.body.password);
 
-//     const validPassword = await userData.checkPassword(req.body.password);
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
 
-//     if (!validPassword) {
-//       res
-//         .status(400)
-//         .json({ message: 'Incorrect email or password, please try again' });
-//       return;
-//     }
-
-//     req.session.save(() => {
-//       req.session.user_id = userData.id;
-//       req.session.logged_in = true;
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
       
-//       res.json({ user: userData, message: 'You are now logged in!' });
-//     });
+      res.json({ user: userData, message: 'You are now logged in!' });
+    });
 
-//   } catch (err) {
-//     res.status(400).json(err);
-//   }
-// });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 
-// router.post('/logout', (req, res) => {
-//   if (req.session.logged_in) {
-//     req.session.destroy(() => {
-//       res.status(204).end();
-//     });
-//   } else {
-//     res.status(404).end();
-//   }
-// });
+router.post('/logout', (req, res) => {
+  if (req.session.logged_in) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
 
 module.exports = router;
--- models/index.js
+++ models/index.js
@@ -4,22 +4,14 @@ const Rating = require('./Rating');
 //SOMEONE PLEASE REVIEW THESE AND CONFRIM
 
 //one to many 
-// User.belongsTo(Rating,{
-//   foreignKey: 'nanny_id'
-// })
-//
-User.hasMany(Rating,{
+User.belongsTo(Rating,{
   foreignKey: 'nanny_id'
 })
 
-// User.belongsTo(Rating,{
-//   foreignKey:'parent_id'
-// })
-
 //rating has the foreign key called parent id
-// Rating.hasOne(User,{
-//   foreignKey: 'parent_id'
-// })
+Rating.hasOne(User,{
+  foreignKey: 'parent_id'
+})
 
 
 module.exports = { Rating, User };
