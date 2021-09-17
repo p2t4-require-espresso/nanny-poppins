061dfd78ee816109e2e79e0a5206dd1e094af1f2
Janet Iqal <j.iqal35@gmail.com>
2021-09-17 13:26:35 -0500

    Update index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const nannyRoutes = require('./nannyRoutes');
+
 const reviewRoutes = require('./reviewRoutes');
 
 
 router.use('/users', userRoutes);
-router.use('/nannys', nannyRoutes);
+
 router.use('/reviews', reviewRoutes);
 
 module.exports = router;
