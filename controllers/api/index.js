14be87dce3bd0adef27dfcc3730de394892a3304
Karla Goo <karla_goo@yahoo.com>
2021-09-22 18:19:57 -0500

    added communication models and routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-
 const reviewRoutes = require('./reviewRoutes');
+const communicationRoutes = require('./communicationRoutes');
 
 
 router.use('/users', userRoutes);
-
+router.use('/communication', communicationRoutes);
 router.use('/reviews', reviewRoutes);
 
 module.exports = router;
